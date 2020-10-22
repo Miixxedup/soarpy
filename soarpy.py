d7e5439a7520a7b883359bf7a688093f5f2d2bc2
Miixxedup <rikdolfing@gmail.com>
2020-10-22 10:44:34 +0200

    Added Todo list

--- soarpy.py
+++ soarpy.py
@@ -1,27 +1,5 @@
-from parsers import parse_playbook
-from core import state
-from tasks import *
 
-# Execute scripts from tasks folder when called.
-def execute_script(name, inputs):
-    tmp = eval(name)
-    values = tmp.task(inputs)
-    return values
+from backend.tasks import *
 
 # Mainloop which contains all the logic for a SOARPY instance.
 def main():
-
-    # Gather all instances to run
-    sequence = parse_playbook.parse_playbook("playbooks/test.spy")
-    
-    for g in sequence:
-        if g == "start":
-            inputs = "ENTER VARIABLES HERE"
-        else:    
-            inputs = state.STATE_DICT[state.PREVIOUS_STATE]["OUTPUT"]
-        result = execute_script(g, inputs)
-        state.add_state(g,inputs, result)
-        state.PREVIOUS_STATE = g
-
-    state.show_states()
-main()
---
+++ todolist.md
@@ -0,0 +1,5 @@
+# Todolist (shortlist)
+- ~~Load all modules from a folder.~~
+- View required attributes which should be available for customisation.
+- Think of a structure to link everything together.
+
